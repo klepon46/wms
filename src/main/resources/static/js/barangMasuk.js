1214ae2b691a8a72c5c5b1cc8ad27d9f01e50edb
garya <klepon.boy14@gmail.com>
2021-09-24 00:19:11 +0800

    dynamic delete

--- src/main/resources/static/js/barangMasuk.js
+++ src/main/resources/static/js/barangMasuk.js
@@ -2,12 +2,20 @@ $(function() {
     let lineNo = 1;
     $("#btnTambahIn").click(function(){
 
-        markup = "<tr><td>This is row "
-                            + lineNo + "</td></tr>";
-                        tableBody = $("table tbody");
-                        tableBody.append(markup);
-                        lineNo++;
+        namaBarang = $("#namaBarang").val();
+        qty = $("#qtyBarang").val();
+        actionButton = '<input type="button" class="btn btn-danger delete_in"  value="Hapus" />';
 
+        markup = "<tr><td>" + lineNo + "</td><td>" + namaBarang+"</td><td>"+qty+"</td><td>" +actionButton+"</td></tr>";
+        tableBody = $("table tbody");
+        tableBody.append(markup);
+        lineNo++;
+
+    });
+
+    $( "#table1" ).on( "click", ".delete_in", function(e) {
+       e.preventDefault();
+       $( this ).parents( "tr" ).remove();
     });
 
 });
